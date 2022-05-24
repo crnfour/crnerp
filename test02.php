f2155cd45bf5fb2783791867cb2f3959b9ff5979
crnfour <steffen@cornehl.biz>
2022-05-24 14:57:43 +0200

    Änderung test02.php

---
+++ test02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Überschrift</h1>
+        <p>das ist ein Test für ein Git file</p>
+
+    </div>    
+
+
+</body>
+</html>
