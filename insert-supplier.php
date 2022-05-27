95128a38e99ddedb81801e940b489274f8b88b8a
crnfour <steffen@cornehl.biz>
2022-05-27 06:05:32 +0200

    pull updates

--- insert-supplier.php
+++ insert-supplier.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />   
+
 <title>Insert Supplyer</title>
 	
 		<link rel="stylesheet" type="text/css" href="mystyle.css">
